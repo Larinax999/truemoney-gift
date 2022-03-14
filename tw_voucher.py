20f235f3dfffda97ce86d90e12f1f62adda5cc1a
Larina <49904492+Larinax999@users.noreply.github.com>
2022-03-14 14:46:38 +0700

    how

--- tw_voucher.py
+++ tw_voucher.py
@@ -14,8 +14,7 @@ class SSLFactory:
         context.set_ciphers(ciphers)
         return context
 
-sslgen = SSLFactory()
-sess = Client(verify=sslgen(),headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67"})
+sess = Client(verify=SSLFactory()(),headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67"})
 def voucher(v:str,num:str):
     v = v.replace('/','').split('v=')[-1]
     if 18 != len(v): return {"succes": False,"msg":"INVAILD_VOUCHER"}
